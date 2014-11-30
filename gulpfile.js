cf589c8b4fd74194e02b781753869e22fbe5513a
ttbarnes <tony@tonybarnes.me>
2014-11-30 20:18:44 +0000

    gulp init - sass compile, npm dependencies

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'));
+
+
+
+});
+
+gulp.task('default', ['sass']);
