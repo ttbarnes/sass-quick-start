6f1a3e9e204e9146ccc843d80dc6b2c230f39405
ttbarnes <tony@tonybarnes.me>
2014-12-08 20:58:26 +0000

    css concat, minify, rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+    concat = require('gulp-concat');
+    minifyCSS = require('gulp-minify-css');
+    rename = require('gulp-rename');
 
-gulp.task('sass', function() {
-  gulp.src('scss/*.scss')
+gulp.task('sass', function () {
+  gulp.src('scss/**/*.scss')
     .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./'))
+    .pipe(minifyCSS())
+    .pipe(rename('main.min.css'))
     .pipe(gulp.dest('dist/css'));
-
-
-
 });
 
 gulp.task('default', ['sass']);
