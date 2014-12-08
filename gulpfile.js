bce8ff7d59b32677916d484bb6592f34952705dd
ttbarnes <tony@tonybarnes.me>
2014-12-08 21:11:54 +0000

    gulp watch, mini clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
-    concat = require('gulp-concat');
-    minifyCSS = require('gulp-minify-css');
-    rename = require('gulp-rename');
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch');
 
 gulp.task('sass', function () {
   gulp.src('scss/**/*.scss')
@@ -13,6 +14,10 @@ gulp.task('sass', function () {
     .pipe(minifyCSS())
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
 });
 
 gulp.task('default', ['sass']);
